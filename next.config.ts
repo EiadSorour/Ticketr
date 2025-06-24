b03d198b0c1758920572b11867b01af26ca68ff3
Your Name <you@example.com>
2025-06-24 03:34:27 +0300

    creat_and_edit_event_implemented

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      { hostname: "zealous-clownfish-33.convex.cloud", protocol: "https" },
+    ],
+  },
 };
 
 export default nextConfig;
